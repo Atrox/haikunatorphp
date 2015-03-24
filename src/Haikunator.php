aa8e4aa0c5a7c9a11378baa15b599cd26bd3ebcd
Atrox <mail@atrox.me>
2015-03-24 19:31:10 +0100

    move adjs & nouns
    better overall view

--- src/Haikunator.php
+++ src/Haikunator.php
@@ -4,6 +4,42 @@ namespace Atrox\Haikunator;
 
 class Haikunator
 {
+    private static $ADJECTIVES = [
+        "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
+        "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
+        "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
+        "billowing", "broken", "cold", "damp", "falling", "frosty", "green",
+        "long", "late", "lingering", "bold", "little", "morning", "muddy", "old",
+        "red", "rough", "still", "small", "sparkling", "throbbing", "shy",
+        "wandering", "withered", "wild", "black", "young", "holy", "solitary",
+        "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine",
+        "polished", "ancient", "purple", "lively", "nameless", "lucky", "odd", "tiny",
+        "free", "dry", "yellow", "orange", "gentle", "tight", "super", "royal", "broad",
+        "steep", "flat", "square", "round", "mute", "noisy", "hushy", "raspy", "soft",
+        "shrill", "rapid", "sweet", "curly", "calm", "jolly", "fancy", "plain", "shinny"
+    ];
+
+    private static $NOUNS = [
+        "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
+        "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
+        "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
+        "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
+        "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
+        "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
+        "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
+        "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper",
+        "frog", "smoke", "star", "atom", "band", "bar", "base", "block", "boat",
+        "term", "credit", "art", "fashion", "truth", "disk", "math", "unit", "cell",
+        "scene", "heart", "recipe", "union", "limit", "bread", "toast", "bonus",
+        "lab", "mud", "mode", "poetry", "tooth", "hall", "king", "queen", "lion", "tiger",
+        "penguin", "kiwi", "cake", "mouse", "rice", "coke", "hola", "salad", "hat"
+    ];
+
+    /**
+     * Generate Heroku-like random names to use in your applications.
+     * @param array $params
+     * @return string
+     */
     public static function haikunate(array $params = array())
     {
         $defaults = [
@@ -15,43 +51,10 @@ class Haikunator
 
         $params = array_merge($defaults, $params);
 
-        $ADJECTIVES = [
-            "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
-            "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
-            "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
-            "billowing", "broken", "cold", "damp", "falling", "frosty", "green",
-            "long", "late", "lingering", "bold", "little", "morning", "muddy", "old",
-            "red", "rough", "still", "small", "sparkling", "throbbing", "shy",
-            "wandering", "withered", "wild", "black", "young", "holy", "solitary",
-            "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine",
-            "polished", "ancient", "purple", "lively", "nameless", "lucky", "odd", "tiny",
-            "free", "dry", "yellow", "orange", "gentle", "tight", "super", "royal", "broad",
-            "steep", "flat", "square", "round", "mute", "noisy", "hushy", "raspy", "soft",
-            "shrill", "rapid", "sweet", "curly", "calm", "jolly", "fancy", "plain", "shinny"
-        ];
-        $NOUNS = [
-            "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
-            "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
-            "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
-            "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
-            "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
-            "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
-            "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
-            "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper",
-            "frog", "smoke", "star", "atom", "band", "bar", "base", "block", "boat",
-            "term", "credit", "art", "fashion", "truth", "disk", "math", "unit", "cell",
-            "scene", "heart", "recipe", "union", "limit", "bread", "toast", "bonus",
-            "lab", "mud", "mode", "poetry", "tooth", "hall", "king", "queen", "lion", "tiger",
-            "penguin", "kiwi", "cake", "mouse", "rice", "coke", "hola", "salad", "hat"
-        ];
+        if ($params["tokenHex"] == true) $params["tokenChars"] = "0123456789abcdef";
 
-        if ($params["tokenHex"] == true)
-        {
-            $params["tokenChars"] = "0123456789abcdef";
-        }
-
-        $adjective = $ADJECTIVES[mt_rand(0, count($ADJECTIVES) - 1)];
-        $noun = $NOUNS[mt_rand(0, count($NOUNS) - 1)];
+        $adjective = self::$ADJECTIVES[mt_rand(0, count(self::$ADJECTIVES) - 1)];
+        $noun = self::$NOUNS[mt_rand(0, count(self::$NOUNS) - 1)];
         $token = "";
 
         for($i = 0; $i <= $params["tokenLength"] - 1; $i++)
